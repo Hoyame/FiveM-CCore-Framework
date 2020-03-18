136711f31c7d68afbf00d25150632a54d6a4e206
Hoyame Z <zou.hoyame@gmail.com>
2020-03-18 18:23:12 +0100

    up corazon_connexion

---
+++ corazon_connexion/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'adamant'
+game 'gta5'
+
+server_script '@mysql-async/lib/MySQL.lua'
+
+client_scripts {
+	'@corazon_core/callback/cl_callback.lua',
+	----------------------------------------------
+	'@corazon_core/core/sh_menu.lua',
+	'@corazon_core/core/sh_functions.lua',
+	'@corazon_core/core/cl_skinchanger.lua',
+	'@corazon_core/core/cl_players.lua',
+	'@corazon_core/core/cl_game.lua'
+	
+}
+
+server_scripts {
+	'@corazon_core/callback/sv_callback.lua',
+	----------------------------------------------
+	'@corazon_core/core/sv_players.lua',
+	'@corazon_core/core/sv_game.lua'
+}
+
+client_scripts {
+	'game/cl_connection.lua'
+}
+
+server_scripts {
+	'game/sv_connection.lua'
+}
